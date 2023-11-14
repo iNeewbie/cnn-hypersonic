36fdc96ba1ac02d3ede908258a6d8cd6c095a99f
Guilherme <52758329+iNeewbie@users.noreply.github.com>
2023-11-14 11:14:03 -0300

    atualização das loss functions

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -11,6 +11,10 @@ from tensorflow.keras.activations import swish
 from tensorflow.keras.losses import MeanSquaredError
 import tensorflow as tf
 import numpy as np
+from keras.optimizers import SGD
+from tensorflow.keras import backend as K
+sgd = SGD(lr=0.01)
+
 
 def trainNeuralNetwork(x_train,conditions_train,y1,y2,epochs_N,batch_size_N):
 
@@ -42,9 +46,53 @@ def trainNeuralNetwork(x_train,conditions_train,y1,y2,epochs_N,batch_size_N):
     pressure = Lambda(lambda x: x[:, :, :, 1:2])(output)
 
     
+    lambda_l2 = 0.01
+    
+    
+    def MSEshared(y_true, y_pred):
+        m = tf.shape(y_true)[0]
+        nx = tf.shape(y_true)[1]
+        ny = tf.shape(y_true)[2]
+        U_true, V_true = tf.split(y_true, 2, axis=-1)
+        U_pred, V_pred = tf.split(y_pred, 2, axis=-1)
+        U_diff = K.square(U_true - U_pred)
+        V_diff = K.square(V_true - V_pred)
+        sum_diff = K.sum(U_diff[:, 1:-1, 1:-1] + V_diff[:, 1:-1, 1:-1])
+        loss = sum_diff / tf.cast(m * (nx - 2) * (ny - 2), tf.float32)
+        return loss
+    
+    def GSshared(y_true, y_pred):
+        m = tf.shape(y_true)[0]
+        nx = tf.shape(y_true)[1]
+        ny = tf.shape(y_true)[2]
+        U_true, V_true = tf.split(y_true, 2, axis=-1)
+        U_pred, V_pred = tf.split(y_pred, 2, axis=-1)
+        dUdx_true = U_true[:, 2:, :] - U_true[:, :-2, :]
+        dUdx_pred = U_pred[:, 2:, :] - U_pred[:, :-2, :]
+        dVdx_true = V_true[:, 2:, :] - V_true[:, :-2, :]
+        dVdx_pred = V_pred[:, 2:, :] - V_pred[:, :-2, :]
+        dU_diff = K.square(dUdx_true - dUdx_pred)
+        dV_diff = K.square(dVdx_true - dVdx_pred)
+        sum_diff = K.sum(dU_diff[:, 1:-1, 1:-1] + dV_diff[:, 1:-1, 1:-1])
+        loss = sum_diff / tf.cast(6 * m * (nx - 2) * (ny - 2), tf.float32)
+
+        return loss
+    
+    def L2regularization(theta, lambda_l2):
+        l2_reg = 0
+        for weight in theta:
+            l2_reg += tf.reduce_sum(tf.square(weight))
+        return lambda_l2 * l2_reg / 2
+
 
     
-    
+    def get_total_loss(model, lambda_l2):
+        def total_loss(y_true, y_pred):
+            return MSEshared(y_true, y_pred) + GSshared(y_true, y_pred) + L2regularization(model.trainable_weights, lambda_l2)
+        return total_loss
+
+
+    """
     def MSEshared(y_true, y_pred):
         Ttruth, Ptruth = tf.split(y_true, 2, axis=-1)
         Tpred, Ppred = tf.split(y_pred, 2, axis=-1)
@@ -57,21 +105,22 @@ def trainNeuralNetwork(x_train,conditions_train,y1,y2,epochs_N,batch_size_N):
         dxdy_pred = tf.gradients(y_pred, [input_img])[0]
         return 6 * m * (n - 1) * tf.reduce_mean(tf.square(dxdy_truth - dxdy_pred))
     
-    def L2regularization(theta):
-        m = tf.shape(theta)[0]
-        return tf.reduce_sum(tf.square(theta)) / m
+    def L2regularization(theta, lambda_l2):
+        l2_reg = 0
+        for weight in theta:
+            l2_reg += tf.reduce_sum(tf.square(weight))
+    return lambda_l2 * l2_reg
     
     def total_loss(y_true, y_pred):
-        theta = autoencoder.trainable_weights
-        return MSEshared(y_true, y_pred)  + L2regularization(theta)
-    
+        return MSEshared(y_true, y_pred) + GSshared(y_true, y_pred) + L2regularization(autoencoder.trainable_weights, lambda_l2)
+    """
     #+ Gshared(y_true, y_pred)
     
     # Compilando o modelo
     autoencoder = Model(inputs=[input_img, input_conditions ], outputs=[temperature,pressure])
     autoencoder.summary()
 
-    autoencoder.compile(optimizer='sgd', loss=MeanSquaredError()) # Usando o otimizador SGD e a função de perda total definida no artigo
+    autoencoder.compile(optimizer=sgd, loss=get_total_loss(autoencoder, lambda_l2)) # Usando o otimizador SGD e a função de perda total definida no artigo
     
     
     history = autoencoder.fit([x_train,conditions_train], [y1,y2], epochs=epochs_N, batch_size=batch_size_N)
