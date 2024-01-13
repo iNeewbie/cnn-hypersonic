ecae7d03b9a7f20ef936610fe6a18d1a122aadbf
Guilherme <52758329+iNeewbie@users.noreply.github.com>
2024-01-13 13:22:53 -0300

    Update treinaRedeNeural.py

--- treinaRedeNeural.py
+++ treinaRedeNeural.py
@@ -61,7 +61,7 @@ lr = 0.1
 filtros = 300
 
 tensorboard_callback = TensorBoard(log_dir='logs')
-checkpoint = ModelCheckpoint('meu_modelo.keras', period=100)
+checkpoint = ModelCheckpoint('meu_modelo.keras', save_freq=200)
 
 #my_callbacks = [tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',factor=0.8,patience=200), tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100,min_delta = 0.001), tf.keras.callbacks.TerminateOnNaN()]
 my_callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=200,min_delta = 0.0001), tf.keras.callbacks.TerminateOnNaN(), tensorboard_callback, checkpoint]
