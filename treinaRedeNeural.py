462ada9d3e3724942fd89388fdbce9703e5fb1ef
iNeewbie <guilherme.afp@outlook.com>
2024-01-18 22:05:29 +0000

    teste com huber 3

--- treinaRedeNeural.py
+++ treinaRedeNeural.py
@@ -45,20 +45,20 @@ except:
     
 
 
-epochs_N = 10000
+epochs_N = 5000
 batch_size_N = 77
 lambda_mse=0.0
 lambda_gs=0.6
 lambda_l2=1e-6
 lambda_huber=0.9
-lr = 0.1
-filtros = 300
+lr = 0.05
+filtros = 100
 
 tensorboard_callback = TensorBoard(log_dir='logs')
 checkpoint = ModelCheckpoint('meu_modelo.keras', save_freq=200)
 
 #my_callbacks = [tf.keras.callbacks.ReduceLROnPlateau(monitor='loss',factor=0.8,patience=200), tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100,min_delta = 0.001), tf.keras.callbacks.TerminateOnNaN()]
-my_callbacks = [tf.keras.callbacks.EarlyStopping(monitor='loss', patience=200,min_delta = 0.0001), tf.keras.callbacks.TerminateOnNaN()]
+my_callbacks = [tf.keras.callbacks.EarlyStopping(monitor='loss', patience=1000,min_delta = 0.0001), tf.keras.callbacks.TerminateOnNaN()]
 
 
 try:
