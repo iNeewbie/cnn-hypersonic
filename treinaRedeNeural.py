0df6fc7f719337449a2922dfe172eaf1ca530637
iNeewbie <guilherme.afp@outlook.com>
2024-01-19 21:02:53 +0000

    teste com mais gdl

--- treinaRedeNeural.py
+++ treinaRedeNeural.py
@@ -47,12 +47,12 @@ except:
 
 epochs_N = 2000
 batch_size_N = 77
-lambda_mse=0.9
-lambda_gs=0.1
+lambda_mse=1
+lambda_gs=0.3
 lambda_l2=1e-6
 lambda_huber=0.0
-lr = 0.001
-filtros = 300
+lr = 0.01
+filtros = 150
 
 tensorboard_callback = TensorBoard(log_dir='logs')
 checkpoint = ModelCheckpoint('meu_modelo.keras', save_freq=200)
