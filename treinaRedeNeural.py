36ae76146bc07703c43f3c81b503a5bc960cf103
iNeewbie <guilherme.afp@outlook.com>
2024-01-19 02:39:45 +0000

    tste com gdl bem baixo

--- treinaRedeNeural.py
+++ treinaRedeNeural.py
@@ -48,10 +48,10 @@ except:
 epochs_N = 500
 batch_size_N = 77
 lambda_mse=1
-lambda_gs=1
+lambda_gs=0.01
 lambda_l2=0#1e-5
 lambda_huber=0.0
-lr = 0.0001
+lr = 0.01
 filtros = 100
 
 tensorboard_callback = TensorBoard(log_dir='logs')
