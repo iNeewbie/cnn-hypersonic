38346bebef08c6d1131a51ea0ec6eead15a4acf7
iNeewbie <guilherme.afp@outlook.com>
2024-12-06 17:47:04 -0300

    30k, bayes, proj2

--- queueScript.py
+++ queueScript.py
@@ -2,6 +2,11 @@ import subprocess
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 import gpustat
+import os
+import logging
+
+# Configuração do logging para registrar mensagens no arquivo output.log
+logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 # Função para verificar o uso da GPU com gpustat
 def verificar_gpu():
@@ -12,23 +17,31 @@ def verificar_gpu():
         # Itera sobre todas as GPUs e verifica o uso da primeira
         for gpu in gpus.gpus:
             gpu_usage = gpu.utilization  # Percentual de uso da GPU
-            print(f"Uso da GPU {gpu.index}: {gpu_usage}%")
+            logging.info(f"Uso da GPU {gpu.index}: {gpu_usage}%")
             
             # Se o uso da GPU for abaixo de 20%, executa o comando desejado
             if gpu_usage < 20:
-                print("GPU com uso abaixo de 20%, executando o script Python...")
-                subprocess.run(['python', 'bayesianOptimization.py'])
+                logging.info("GPU com uso abaixo de 20%, executando o script Python...")
+
+                # Executa o script bayesianOptimization.py e espera ele terminar
+                result = subprocess.run(['python', 'bayesianOptimization.py'])
+
+                # Se o script foi executado com sucesso, encerra o script principal
+                if result.returncode == 0:
+                    logging.info("Execução de bayesianOptimization.py concluída, encerrando o queueScript.py.")
+                    # Encerra o script imediatamente
+                    os._exit(0)  # Encerra o script imediatamente
             else:
-                print("GPU com uso acima de 20%, aguardando...")
+                logging.info("GPU com uso acima de 20%, aguardando...")
 
     except Exception as e:
-        print(f"Erro ao verificar o uso da GPU: {e}")
+        logging.error(f"Erro ao verificar o uso da GPU: {e}")
 
 # Cria o agendador
 scheduler = BackgroundScheduler()
 
-# Agendar a execução da função de verificação da GPU a cada 10 segundos
-scheduler.add_job(verificar_gpu, 'interval', seconds=60)
+# Agendar a execução da função de verificação da GPU a cada 60 segundos
+scheduler.add_job(verificar_gpu, 'interval', seconds=60*20)
 
 # Iniciar o agendador
 scheduler.start()
